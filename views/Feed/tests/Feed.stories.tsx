4edcfc0932093967ccfa518adb2f9c4a44c7c891
Henno <fourie.henno@gmail.com>
2022-10-02 22:37:09 +0200

    initial commit

---
+++ views/Feed/tests/Feed.stories.tsx
@@ -0,0 +1,8 @@
+import { Feed } from '../Feed'
+import { MockContainer } from '../../../store/tests/store.mock'
+
+export default {
+    title: 'views/Feed'
+}
+
+export const Default = () => <MockContainer><Feed /></MockContainer>
